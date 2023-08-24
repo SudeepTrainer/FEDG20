fe7a44b7083cd79c3952bb928169c3a387c57459
SudeepTrainer <info@icenbake.com>
2023-08-24 20:50:43 +0530

    24-08-2023

--- Javascript/functions.js
+++ Javascript/functions.js
@@ -10,3 +10,46 @@ let product = function multiply(a,b){
     return a * b;
 }
 console.log(product(4,5));
+
+// let square = function square(a){
+//     return a * a;
+// }
+
+// console.log(square(4));
+
+// Anonymous function
+let square = function(a){
+    return a * a;
+}
+
+console.log(square(4));
+
+// let sum = function add(a,b){
+//     return a + b;
+// } 
+//Step 1: Remove the function name if it exists
+// let sum = function (a,b){
+//     return a + b;
+// } 
+//Step 2: Remove the function keyword and add =>(fat arrow)
+// let sum = (a,b) => {
+//     return a + b;
+// } 
+// console.log(sum(3,4));
+//Step 3: Remove the curly brace and return if it is the only statement
+let sum = (a,b) => a + b;
+
+// let cube = function(a){
+//     return a*a*a;
+// } 
+// console.log(cube(4));
+// Remove the function keyword and add fat arrow(=>)
+// let cube = (a) => {
+//     return a*a*a;
+// } 
+//Remove the curly brace and return if it is the only statement
+// let cube = (a) => a*a*a ;
+// console.log(cube(4));
+// Remove the paranthesis if there is only 1 param
+let cube = a => a*a*a ;
+console.log(cube(4));
--- Javascript/object.js
+++ Javascript/object.js
@@ -19,3 +19,41 @@ console.log(person);
         price:345435,
         quantity:1
     }];
+
+// object are references
+
+let name1 = "Mark";
+let name2 = name1;
+let name3 = name2;
+
+name2 = "Elon";
+console.log(name3);
+
+let product1 ={
+    name:"laptop",
+    price:43535
+}
+let product2 = {
+    name:"laptop",
+    price:43535
+}
+console.log(product1 === product2);
+product3 = product1;
+product3.name = "keyboard";
+console.log(product1.name);
+
+// clone an object
+
+// let product4 = Object.assign({},product3);
+// console.log(product3);
+// console.log(product4);
+// product3.name = "TV";
+// console.log(product3);
+// console.log(product4);
+
+let product4 ={...product3};
+console.log(product3);
+console.log(product4);
+product3.name = "TV";
+console.log(product3);
+console.log(product4);
